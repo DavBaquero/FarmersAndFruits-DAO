874f132aa6a4eb4ff1bc922550c589d946597241
DavBaquero <david.baquero.19@gmail.com>
2024-06-11 13:43:56 +0200

    🔧 feat(ServiceFruit) : add new function

--- src/main/java/edu/badpals/repositorio/ServiceFruit.java
+++ src/main/java/edu/badpals/repositorio/ServiceFruit.java
@@ -1,6 +1,7 @@
 package edu.badpals.repositorio;
 
 import java.util.List;
+import java.util.Optional;
 
 import edu.badpals.dominio.*;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -20,5 +21,21 @@ public class ServiceFruit {
     public List<Fruit> list(){
         List<Fruit> listaFrutas = fruitRepo.listAll();
         return listaFrutas;
+    }
+
+    public void add(Fruit fruit){
+        Optional<Farmer> posifarmero = Optional.of(farmerRepo.find("name = ?1", fruit.getFarmer().getName()).firstResult());
+        Fruit fruta = null;
+        Farmer farmero = null;
+        if(posifarmero.isPresent()){
+            farmero = posifarmero.get();
+        } else{
+            Farmer nFarmero = new Farmer(fruit.getFarmer().getName(), fruit.getFarmer().getLocation());
+            farmero = nFarmero;
+            farmerRepo.persist(farmero);
+        }
+
+        fruta = new Fruit(fruit.getName(),fruit.getDescription(),farmero);
+        fruitRepo.persist(fruta);
     }
 }
