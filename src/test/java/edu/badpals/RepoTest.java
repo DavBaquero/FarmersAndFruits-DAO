dbd56db832eada47beb41dcd697ac7a27034def2
DavBaquero <david.baquero.19@gmail.com>
2024-06-11 12:33:47 +0200

    🛡️ test(RepoTest.java) : add new test

--- src/test/java/edu/badpals/RepoTest.java
+++ src/test/java/edu/badpals/RepoTest.java
@@ -1,5 +1,5 @@
 
-/* 
+
 package edu.badpals;
 
 
@@ -26,7 +26,7 @@ public class RepoTest {
         Assertions.assertThat(fruta.toString()).contains("Winter fruit"); // item_quality
         Assertions.assertThat(fruta.getId()).isEqualTo(1000);
     }
-
+/* 
     // @Test de jupiter, no el de junit
     @Test
     public void testList() {
